6de85f6a0d11c8f901882df3685ccbe5c9cb2cd8
Moon <mjskks94@naver.com>
2023-01-13 19:58:12 +0900

    다중 step 구성

--- src/main/java/com/springboot/batch/demo/config/ExampleJobConfig.java
+++ src/main/java/com/springboot/batch/demo/config/ExampleJobConfig.java
@@ -1,6 +1,7 @@
 package com.springboot.batch.demo.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -10,6 +11,7 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 @EnableBatchProcessing
@@ -22,18 +24,40 @@ public class ExampleJobConfig {
     @Bean
     public Job ExampleJob() {
         Job exampleJob = jobBuilderFactory.get("exampleJob")
-                .start(step())
+                .start(startStep())
+                .next(nextStep())
+                .next(lastStep())
                 .build();
 
         return exampleJob;
     }
 
     @Bean
-    public Step step() {
+    public Step startStep() {
         return stepBuilderFactory
-                .get("step")
+                .get("startStep")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step");
+                    log.info("start step!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step nextStep() {
+        return stepBuilderFactory.get("nextStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("next step!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step lastStep() {
+        return stepBuilderFactory.get("lastStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("last step!");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
